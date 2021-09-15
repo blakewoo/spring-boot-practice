4269b3d63715397dbce14e4f1ceb33e376bbbbe9
Blake <blakewoo0819@gmail.com>
2021-09-15 23:21:28 +0900

    test commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+    //just test
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
