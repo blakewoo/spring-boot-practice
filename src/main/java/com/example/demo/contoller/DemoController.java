ef9464da22c63a54481adb540deefa5201d6ea1d
Blake <blakewoo0819@gmail.com>
2021-09-21 18:25:11 +0900

    template & reponse body study

--- src/main/java/com/example/demo/contoller/DemoController.java
+++ src/main/java/com/example/demo/contoller/DemoController.java
@@ -3,6 +3,8 @@ package com.example.demo.contoller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
@@ -11,5 +13,17 @@ public class DemoController {
     public String hello(Model model) {
       model.addAttribute("data","hello!");
       return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
     }
 }
